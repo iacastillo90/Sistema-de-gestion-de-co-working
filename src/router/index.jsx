3e4a8b41349fbecd830aa99d3d921d52c2ba4170
Ivan Castillo <137194587+iacastillo90@users.noreply.github.com>
2026-04-05 03:52:05 -0400

    feat: mejora de navegación con NavLink y página 404
    Detalles:
    - Implementación de ruta comodín (*) y componente NotFound para manejo de errores 404.
    - Refactorización de Header.jsx utilizando NavLink para detectar rutas activas.
    - Reemplazo de etiquetas <a> estáticas por componentes <Link> para navegación interna sin recargas.

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,7 @@ import Home from '../pages/Home/Home.jsx';
 import Login from '../pages/Login/Login.jsx';
 import MainLayout from '../layouts/MainLayout.jsx';
 import Memberships from '../pages/Memberships/Memberships.jsx';
+import NotFound from '../pages/NotFound/NotFound.jsx';
 import { createBrowserRouter } from 'react-router-dom'
 
 export const router = createBrowserRouter([
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
             {
                 path: 'membresias',
                 element: <Memberships />,
+            },
+            {
+                path: "*",
+                element: <NotFound />
             }
         ],
     },
