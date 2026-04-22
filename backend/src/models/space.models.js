be8326ca63607320256d9dac44a8c637fb11c29f
Ivan Castillo <137194587+iacastillo90@users.noreply.github.com>
2026-04-22 15:37:55 -0400

    Ultima version de migracion con Crud de User y MongoDB

--- backend/src/models/space.models.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs/promises');
-const path = require('path');
-const spacesFilePath = path.join(__dirname, '../data/spaces.json');
-
--- backend/src/routers/space.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getSpaces } = require('../controller/spaces.controller');
-
-// Ruta para obtener todos los espacios
-router.get('/', getSpaces);
-
-module.exports = router;
-
